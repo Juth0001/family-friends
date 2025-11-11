2ef2b2b630f1b62344c6b394c697922a595172e7
Julie <juth0001@stud.kea.dk>
2025-11-11 14:54:25 +0100

    hulter tilbulder

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,37 @@
+"use client";
+import Image from "next/image";
+import productsData from "./data/products.json"; 
+
+
+export default function ProductList(){
+    const products = productsData.products;
+
+    return(
+<section className="grid grid-cols-2 gap-4 justify-center "> 
+{products.map((product) =>(
+    <div key={product.id}
+    className="shadow-md rounded-2xl overflow-hidden">
+        
+        <Image
+            src={product.thumbnail}
+            alt={product.title}
+            width={339}
+            height={360}
+            className="rounded-xl mb-4"
+          />
+          <div className="px-4 pb-4 flex justify-between"> 
+            <h2 className="text-xl font-semibold mb-2">{product.title}</h2>
+            <p className="font-light text-black-4">${product.price}</p>
+          </div>
+          
+          <p className="text-gray-600 text-sm mb-3">{product.description}</p>
+         
+          {/* <button className="">
+            <h5><strong>BUY</strong> {product.type}</h5>
+          </button> */}
+        </div>
+       
+))}
+</section>
+    );
+}
---
+++ app/components/filter-btn.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+
+const filter_btn = () => {
+    return (
+      <button>
+
+      </button>
+    );
+  };
+  
+  export default filter_btn;
